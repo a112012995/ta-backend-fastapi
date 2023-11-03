2b8f189bad69987f8750f2355327da07a23785e5
Ahmad Fajar Kusumajati <111202012995@mhs.dinus.ac.id>
2023-11-03 15:38:53 +0700

    changed env.py

--- alembic/env.py
+++ alembic/env.py
@@ -25,8 +25,8 @@ target_metadata = Base.metadata
 
 def get_url() -> str:
     user = os.getenv("MYSQL_USER", "root")
-    password = os.getenv("MYSQL_PASSWORD", "notfound")
-    server = os.getenv("MYSQL_SERVER", "localhost")
+    password = os.getenv("MYSQL_PASSWORD", "password")
+    server = os.getenv("MYSQL_SERVER", "127.0.0.1")
     db = os.getenv("MYSQL_DB", "ta-db")
     # Use mysql+pymysql driver
     return f"mysql+pymysql://{user}:{password}@{server}/{db}"
